8c2ba18512c2499eadd61cf1fed2c2a7750205da
MartyMen <60930141+MartyMen@users.noreply.github.com>
2023-02-27 11:35:40 +0000

    Update MorseCodeDecoder.java

--- MorseCodeDecoder.java
+++ MorseCodeDecoder.java
@@ -1,20 +1,36 @@
 public class MorseCodeDecoder {
-        public static String decode(String morseCode) {
-            // your brilliant code here, remember that you can access the preloaded Morse code table through MorseCode.get(code)
-            String[] arrOfStr = morseCode.split(" ", 0);
-            String decoder = "";
-            String encoded = "";
-
-            for (int i = 0; i < arrOfStr.length; i++)
+    public static String decode(String morseCode) {
+      String[] arrOfStr = morseCode.split(" ", 0);
+      String decoder = "";
+      String encoded = "";    
+      
+//         for (int i = 0; i < arrOfStr.length; i++)
+//           {
+//             String a = arrOfStr[i];
+//             encoded = MorseCode.get(a);
+//             if(encoded == null && MorseCode.get(arrOfStr[i+1]) == null)
+//               {
+//               encoded = "";
+//               }
+//             decoder = decoder + encoded;
+//           }
+      
+        for (int i = 0; i < arrOfStr.length; i++)
+          {
+            String a = arrOfStr[i];
+            encoded = MorseCode.get(a);
+            if(encoded == null && MorseCode.get(arrOfStr[i+1]) == null)
+              {
+              encoded = "";
+              }
+            else if(encoded == null)
             {
-                String a = arrOfStr[i];
-                encoded = MorseCode.get(a);
-                if(encoded == null)
-                {
-                    encoded = " ";
-                }
-                decoder = decoder + encoded;
+              encoded = " ";
             }
-            return decoder;
-        }
+            decoder = decoder + encoded;
+          }
+      System.out.print(decoder);
+      
+      return decoder;
     }
+}
